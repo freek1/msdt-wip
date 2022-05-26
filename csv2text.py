cc920049fb8282b1575ea1d488dea144f05942b4
Freek <freek.hens@ru.nl>
2022-05-26 17:34:05 +0200

    Add markdown table

--- csv2text.py
+++ csv2text.py
@@ -7,7 +7,7 @@ try:
         txt_file = fn+'.txt'
         with open(txt_file, "w") as my_output_file:
             with open(csv_file, "r") as my_input_file:
-                [ my_output_file.write(" ".join(row)+'\n') for row in csv.reader(my_input_file)]
+                [ my_output_file.write("|"+" | ".join(row)+' |\n') for row in csv.reader(my_input_file)]
             my_output_file.close()
 except:
     print("Error opening files")
